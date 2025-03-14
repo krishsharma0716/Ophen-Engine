6785de3488e162febd39f5a5e17012cf35daac87
ksharma0716 <krishsharma2007@icloud.com>
2025-03-13 21:27:36 -0400

    trying to make AppKit work

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
-#include <cinttypes>
-#include <bitset>
 #include "boardPieceRep.h"
+#include </Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.2.sdk/System/Library/>
 
 int main() {
+    NS::AutoreleasePool
     boardPieceRep::setBoard();
     boardPieceRep::printBoard();
 }
