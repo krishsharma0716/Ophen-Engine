16d581355e829a3d3021a9bdddff3cfde0e3ece2
Krish Sharma <112504404+krishsharma0716@users.noreply.github.com>
2025-03-23 23:39:35 -0400

    Update main.cpp
    got rid of appkit/metal implements because they weren't working. will work on adding cross platform gui for ease

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include "boardPieceRep.h"
-#include </Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.2.sdk/System/Library/>
 
 int main() {
-    NS::AutoreleasePool
     boardPieceRep::setBoard();
     boardPieceRep::printBoard();
 }
